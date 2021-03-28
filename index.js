d48c445daedc5905ee613a0e308fe869f265ad55
Shebraska <lisette.bakker@live.nl>
2021-03-28 20:55:09 +0200

    dsv
    uoiy

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("menu").style.backgroundColor =  "#a1cae2", document.getElementById("wit").style.display = "inline-block", document.getElementById("bars").style.display = "inline-block";}
-  else {
-    document.getElementById("menu").style.backgroundColor = "transparent", document.getElementById("wit").style.display = "none", document.getElementById("bars").style.display = "none";}
-}
 function myMenu() {
   var item = document.querySelectorAll('.item');
   item.forEach(function(it) {
@@ -14,3 +6,11 @@ function myMenu() {
     else {it.classList.add('active'), document.getElementById("menu").style.flexDirection = "column", document.getElementById("menu").style.backgroundColor = "#a1cae2", document.getElementById("toggle").style.paddingRight = "0";}
   })
 };
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("menu").style.backgroundColor =  "#a1cae2", document.getElementById("wit").style.display = "inline-block", document.getElementById("bars").style.display = "inline-block";}
+  else {
+    document.getElementById("menu").style.backgroundColor = "transparent", document.getElementById("wit").style.display = "none", document.getElementById("bars").style.display = "none";}
+}
