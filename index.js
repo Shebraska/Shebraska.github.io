b841f67b2dcf96cb02fbce8359176dfb25d34654
Shebraska <lisette.bakker@live.nl>
2021-03-28 13:54:15 +0200

    pomp
    pomp

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("menu").style.backgroundColor =  "#a1cae2", document.getElementById("wit").style.display = "inline-block", document.getElementById("bars").style.display = "inline-block";}
+  else {
+    document.getElementById("menu").style.backgroundColor = "transparent", document.getElementById("wit").style.display = "none", document.getElementById("bars").style.display = "none";}
+}
+function myMenu() {
+  var item = document.querySelectorAll('.item');
+  item.forEach(function(it) {
+    if (it.classList.contains('active'))
+    {it.classList.remove('active'), document.getElementById("menu").style.flexDirection = "row", document.getElementById("toggle").style.paddingRight = "3rem";}
+    else {it.classList.add('active'), document.getElementById("menu").style.flexDirection = "column", document.getElementById("menu").style.backgroundColor = "#a1cae2", document.getElementById("toggle").style.paddingRight = "0";}
+  })
+};
