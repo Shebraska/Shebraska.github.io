079193e23528c0541cdffabdba70e9b24191ce49
Shebraska <lisette.bakker@live.nl>
2021-04-03 19:09:47 +0200

    sadhhg
    iuytfc

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 function myMenu() {
   var item = document.querySelectorAll('.item');
   item.forEach(function(it) {
-    if (it.classList.contains('active'))
-    {it.classList.remove('active'), document.getElementById("menu").style.flexDirection = "row", document.getElementById("toggle").style.paddingRight = "3rem";}
-    else {it.classList.add('active'), document.getElementById("menu").style.flexDirection = "column", document.getElementById("menu").style.backgroundColor = "#a1cae2", document.getElementById("toggle").style.paddingRight = "0";}
+    if (it.classList.contains('active')) {
+      it.classList.remove('active'), document.getElementById("menu").style.flexDirection = "row", document.getElementById("toggle").style.paddingRight = "3rem";
+    } else {
+      it.classList.add('active'), document.getElementById("menu").style.flexDirection = "column", document.getElementById("toggle").style.paddingRight = "0";
+    }
   })
 };
 
-window.onscroll = function() {scrollFunction()};
+function myItem() {
+  var item2 = document.querySelectorAll('.item');
+  item2.forEach(function(it2) {
+    it2.classList.remove('active');
+    document.getElementById("menu").style.flexDirection = "row";
+    document.getElementById("toggle").style.paddingRight = "3rem";
+  })
+};
+
+window.onscroll = function() {
+  var media = window.matchMedia("(min-width: 1050px)");
+  if (media.matches) {
+    document.getElementById("boven").style.display = "none";
+  } else {
+    scrollFunction()
+  }
+};
+
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("menu").style.backgroundColor =  "#a1cae2", document.getElementById("wit").style.display = "inline-block", document.getElementById("bars").style.display = "inline-block";}
-  else {
-    document.getElementById("menu").style.backgroundColor = "transparent", document.getElementById("wit").style.display = "none", document.getElementById("bars").style.display = "none";}
-}
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("boven").style.display = "flex";
+  } else {
+    document.getElementById("boven").style.display = "none";
+  }
+};
