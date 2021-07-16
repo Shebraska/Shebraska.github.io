653c51976a68fc6097bf8ca6f8648f9a8c9b22be
Shebraska <lisette.bakker@live.nl>
2021-07-16 16:52:59 +0200

    wef938ewusjd

--- index.js
+++
@@ -1,8 +0,0 @@
-function myMenu() {
-  var item = document.querySelectorAll('.item');
-  item.forEach(function(it) {
-    if (it.classList.contains('active'))
-    {it.classList.remove('active')}
-    else {it.classList.add('active')}
-  })
-};
